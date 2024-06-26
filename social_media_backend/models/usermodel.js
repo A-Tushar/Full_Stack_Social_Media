c253b8a77d1f24a78d819734219e2284fc2f47c3
A-Tushar <mishurahman2022@gmail.com>
2024-06-26 13:51:45 +0600

    User Model Created
    User model done

---
+++ social_media_backend/models/usermodel.js
@@ -0,0 +1,159 @@
+const mongoose = require("mongoose");
+const { post } = require("../routes");
+const Schema = mongoose.Schema;
+const  {ObjectId} = mongoose.Schema;
+
+
+
+const UserModel = new Schema(
+    {
+        first_name:{
+            type:String,
+            require:true,
+            trim:true,
+            text:true,
+        },
+        last_name:{
+            type:String,
+            require:true,
+            trim:true,
+            text:true,
+        },
+        user_name:{
+            type:String,
+            require:true,
+            unique:true,
+            trim:true,
+            text:true,
+        },
+        email:{
+            type:String,
+            require:true,
+            unique:true,
+            trim:true,
+            text:true,
+        },
+        password:{
+            type:String,
+            require:true,
+            trim:true,
+            text:true,
+        },
+        profile_pic:{
+            type:String,
+            default: ''
+        },
+        cover_pic:{
+            type:String,
+            default: ''
+        },
+        date_of_birth:{
+            type:Date,
+            require:true,
+        },
+        gender:{
+            type: String,
+            require:true,
+        },
+        verify:{
+            type:Boolean,
+            default:false,
+        },
+        friends:[
+            {
+                type:ObjectId,
+                ref:'userModel'
+            }
+        ],
+        followers:[
+            {
+                type:ObjectId,
+                ref:'userModel'
+            }
+        ],
+        following:[
+            {
+                type:ObjectId,
+                ref:'userModel'
+            }
+        ],
+        request:[
+            {
+                type:ObjectId,
+                ref:'userModel'
+            }
+        ],
+        search:[
+            {
+                user:{
+                    type:ObjectId,
+                    ref:'userModel',
+                    require:true,
+                    text:true
+                },
+                createdAt:{
+                    type:date,
+                    require:true
+                }
+            }
+        ],
+        details:{
+            bio:{
+                type:String
+            },
+            other_name:{
+                type:String
+            },
+            job:{
+                type:String
+            },
+            current_city:{
+                type:String
+            },
+            workplace:{
+                type:String
+            },
+            college:{
+                type:String
+            },
+            highschool:{
+                type:String
+            },
+            home_town:{
+                type:String
+            },
+            relationship:{
+                type:String,
+                enum:[
+                    'Single',
+                    'In a relationship',
+                    'Divorced',
+                    "It's Compicated",
+                    "Married"
+                ]
+            },
+            other_social_connection:{
+                type:String
+            },
+        },
+        save_post:[
+            {
+                post:{
+                    type:ObjectId,
+                    ref:post
+                },
+                savedAt:{
+                    type:date,
+                    require:true
+                }
+            }
+        ]
+
+
+    },
+    {
+        timestamps:true
+    }
+);
+
+module.exports = mongoose.model('usermodel',UserModel);
